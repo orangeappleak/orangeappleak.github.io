cf0373733a3f0e2bd7be5d26b614c70d3ca8d43e
orangeappleak <40416975+orangeappleak@users.noreply.github.com>
2020-04-16 17:03:25 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -49,5 +49,20 @@ $("document").ready(() => {
             $("#underline_about").css({"transform" : "translateX(-102%)"});
         });
     }
+
+    (function() {
+        if ("Notification" in window) {
+          var permission = Notification.permission;
+      
+          if (permission === "denied" || permission === "granted") {
+            return;
+          }
+      
+          Notification.requestPermission().then(function() {
+              var notification = new Notification("Hello, world!");
+            });
+        }
+      })();
 });
+
 
